e7e0441a34c895dab6dbf20db2ceec491943ec7f
turun19 <faturrizqy19@gmail.com>
2023-05-19 15:08:59 +0700

    Initial commit

---
+++ src/main/java/com/binar/challenge4/service/SeatService.java
@@ -0,0 +1,36 @@
+package com.binar.challenge4.service;
+
+import com.binar.challenge4.model.Schedule;
+import com.binar.challenge4.model.Seat;
+import com.binar.challenge4.repository.ScheduleRepository;
+import com.binar.challenge4.repository.SeatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SeatService {
+    @Autowired
+    private SeatRepository seatRepository;
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
+    public List<Seat> getAllSeat(){
+        return seatRepository.findAll();
+
+    }
+
+    public Seat addSeats(String seatNumber, boolean isAvailable, String studioName) throws Exception {
+        Seat seats = seatRepository.findSeatsBySeatNumberAndStudio(seatNumber, studioName);
+        if(seats != null){
+            throw new Exception("Seats already exist");
+        }
+        Seat seat = new Seat();
+        seat.setSeatNumber(seatNumber);
+        seat.setIsAvailable(isAvailable);
+        seat.setStudio(studioName);
+
+        return seatRepository.save(seat);
+    }
+}
